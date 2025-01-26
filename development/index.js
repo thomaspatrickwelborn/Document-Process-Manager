26d0e285cadd27c875c50c44b19b86c3afd33755
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2025-01-26 12:53:19 -0800

    DPM-to-Router Class Property Transfer

--- development/index.js
+++ development/index.js
@@ -1,68 +1,63 @@
 import './coutil/persist/index.js'
 import path from 'node:path'
 import inspector from 'node:inspector'
+import http from 'node:http'
 import https from 'node:https'
-import express from 'express'
 import browserSync from 'browser-sync'
 import Router from './router/index.js'
 export default class DocumentProcessManager extends EventTarget {
   #settings
-  #_inspector
-  #_https
-  #_browserSync
-  #_express
-  #_static
-  #_router
+  #inspector
+  #server
+  #browserSync
+  #router
   constructor($settings) {
     super()
     this.#settings = $settings
     this.inspector
-    this.https
-    this.static
-    this.router
+    this.server
     this.browserSync
   }
   get name() { return this.#settings.name }
   // Node Inspector
   get inspector() {
-    if(this.#_inspector !== undefined) {
-      return this.#_inspector
-    }
-    this.#_inspector = inspector.open(
+    if(this.#inspector !== undefined) { return this.#inspector }
+    this.#inspector = inspector.open(
       this.#settings.inspector.port,
       this.#settings.inspector.host
     )
-    return this.#_inspector
+    return this.#inspector
   }
-  // Node HTTPS Server
-  get https() {
-    if(this.#_https !== undefined) {
-      return this.#_https
+  get server() {
+    if(this.#server !== undefined) { return this.#server }
+    if(this.#settings.server.https) {
+      // Node HTTPS Server
+      this.#server = https.createServer(
+        this.#settings.server.https,
+        this.router.express
+      )
+      this.#server.listen(
+        this.#settings.server.https.port, 
+        this.#settings.server.https.host,
+        ($request, $response) => { /**/ } 
+      )
     }
-    this.#_https = https.createServer(
-      {
-        key: this.#settings.https.key,
-        cert: this.#settings.https.cert,
-      },
-      this.express
-    )
-    this.#_https.listen(
-      this.#settings.https.port, 
-      this.#settings.https.host,
-      ($request, $response) => { /**/ } 
-    )
-    return this.#_https
+    else if(this.server.http) {
+      // Node HTTPS Server
+    }
+    return this.#server
   }
-  get express() {
-    if(this.#_express !== undefined) { return this.#_express }
-    this.#_express = express(this.#settings.express || {})
-    return this.#_express
+  // Router
+  get router() {
+    if(this.#router !== undefined) { return this.#router }
+    if(this.#settings.router !== undefined) {
+      this.#router = new Router(this.#settings.router, this)
+    }
+    return this.#router
   }
   // BrowserSync
   get browserSync() {
-    if(this.#_browserSync !== undefined) {
-      return this.#_browserSync
-    }
+    if(this.#browserSync !== undefined) { return this.#browserSync }
     const browserSyncServerOptions = {
       ui: false, 
       open: false, 
@@ -73,36 +68,15 @@ export default class DocumentProcessManager extends EventTarget {
       proxy: {
         target: [
           "https://",
-          this.#settings.https.host, ":",
-          this.#settings.https.port,
+          this.#settings.server.https.host, ":",
+          this.#settings.server.https.port,
         ].join(''),
         ws: false,
       },
     }
-    this.#_browserSync = browserSync.create()
-    this.#_browserSync.init(browserSyncServerOptions)
+    this.#browserSync = browserSync.create()
+    this.#browserSync.init(browserSyncServerOptions)
     this.dispatchEvent(new CustomEvent('ready', { detail: this }))
-    return this.#_browserSync
-  }
-  get static() {
-    if(this.#_static !== undefined) { return this.#_static }
-    if(this.#settings.static !== undefined) {
-      const staticElements = []
-      for(const [$staticPath, $staticOptions] of this.#settings.static) {
-        const staticElement = express.static(path.join(process.env.PWD, $staticPath), $staticOptions)
-        this.express.use(staticElement)
-        staticElements.push(staticElement)
-      }
-      this.#_static = staticElements
-    }
-    return this.#_static
-  }
-  // Router
-  get router() {
-    if(this.#_router !== undefined) { return this.#_router }
-    if(this.#settings.router !== undefined) {
-      this.#_router = new Router(this.#settings.router, this)
-    }
-    return this.#_router
+    return this.#browserSync
   }
 }
--- development/router/index.js
+++ development/router/index.js
@@ -1,5 +1,6 @@
 import path from 'node:path'
 import { rm, mkdir, readFile } from 'node:fs'
+import express from 'express'
 import { globSync } from 'glob'
 import watch from 'glob-watcher'
 import Route from './route/index.js'
@@ -7,16 +8,41 @@ import RouteMethodNames from '../coutil/routeMethods/index.js'
 export default class Router extends EventTarget {
   length = 0
   #settings
-  #_watcher
+  #dpm
+  #express
+  #static
   #source
   #target
+  #_watcher
   #_boundAdd
   #_boundChange
   #_boundUnlink
-  constructor($settings) {
+  constructor($settings, $dpm) {
     super()
     this.#settings = $settings
+    this.dpm = $dpm
+    this.static
     this.#watcher
+  }
+  get express() {
+    if(this.#express !== undefined) { return this.#express }
+    this.#express = express(this.#settings.express || {})
+    return this.#express
+  }
+
+  get static() {
+    if(this.#static !== undefined) { return this.#static }
+    if(this.#settings.static !== undefined) {
+      const staticElements = []
+      for(const [$staticPath, $staticOptions] of this.#settings.static) {
+        const staticPath = path.join(process.env.PWD, $staticPath)
+        const staticElement = express.static(staticPath, $staticOptions)
+        this.express.use(staticElement)
+        staticElements.push([staticPath, staticElement])
+      }
+      this.#static = staticElements
+    }
+    return this.#static
   }
   get source() {
     if(this.#settings.source !== undefined) return this.#settings.source
