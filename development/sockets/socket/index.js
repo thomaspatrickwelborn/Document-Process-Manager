4f3b31250c506287ca041cc9d845b58d7cff17a1
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2025-02-01 15:19:25 -0800

    Socket Events

--- development/sockets/socket/index.js
+++ development/sockets/socket/index.js
@@ -1,38 +1,28 @@
 import { URL } from 'node:url'
 import { Buffer } from 'node:buffer'
-import WebSocket, { WebSocketServer } from 'ws'
+import { WebSocketServer } from 'ws'
 import MessageAdapter from './messageAdapter/index.js'
 export default class Socket extends EventTarget {
   #settings
   #sockets
   #webSocketServer
-  #webSocket
+  #_webSocket
   #active = false
   #messageAdapters
   #url
-  #_boundConnection
-  #boundOpen
-  #boundClose
-  #boundError
-  #boundMessage
   constructor($settings, $sockets) {
     super()
     this.#settings = $settings
     this.#sockets = $sockets
     this.active = this.#settings.active
-    this.#boundOpen = this.#open.bind(this)
-    this.#boundClose = this.#close.bind(this)
-    this.#boundError = this.#error.bind(this)
-    this.#boundMessage = this.#message.bind(this)
   }
   get active() { return this.#active }
   set active($active) {
-    if(this.#active === $active) { return }
     if($active === true) {
       this.webSocketServer
     }
     else if($active === false) {
-      this.webSocketServer.close()
+      this.webSocketServer?.close()
       this.#webSocketServer = undefined 
       this.#webSocket = undefined
     }
@@ -59,37 +49,38 @@ export default class Socket extends EventTarget {
       path: this.url.pathname,
       server: this.#sockets.server,
     })
-    this.#webSocketServer.on('connection', this.#boundConnection)
+    this.#webSocketServer.on('connection', this.#websocketServerConnection.bind(this))
+    this.#webSocketServer.on('close', this.#websocketServerClose.bind(this))
+    this.#webSocketServer.on('error', this.#websocketServerError.bind(this))
     return this.#webSocketServer
   }
-  get webSocket() { return this.#webSocket }
-  set webSocket($webSocket) {
-    if(this.#webSocket !== undefined) return
-    this.#webSocket = $webSocket
-    this.#webSocket.on('open', this.#boundOpen)
-    this.#webSocket.on('close', this.#boundClose)
-    this.#webSocket.on('error', this.#boundError)
-    this.#webSocket.on('message', this.#boundMessage)
+  get #webSocket() { return this.#_webSocket }
+  set #webSocket($webSocket) {
+    if($webSocket === undefined) { this.#_webSocket = undefined }
+    else {
+      this.#_webSocket = $webSocket
+      this.#_webSocket.on('message', this.#websocketMessage.bind(this))
+      this.#_webSocket.on('error', this.#websocketError.bind(this))
+      this.#_webSocket.on('open', this.#websocketOpen.bind(this))
+      this.#_webSocket.on('close', this.#websocketClose.bind(this))
+    }
   }
-  get #boundConnection() {
-    if(this.#_boundConnection !== undefined) { return this.#_boundConnection }
-    this.#_boundConnection = this.#connection.bind(this)
-    return this.#_boundConnection
-  }
-  #connection($ws) {
-    this.webSocket = $ws
-  }
-  #open($event) { console.log("#open", $event) }
-  #close($event) { console.log("#close", $event) }
-  #error() { console.error("#error", ...arguments) }
-  #message($data, $isBinary) {
+  #websocketServerConnection($ws) { this.#webSocket = $ws }
+  #websocketServerClose() { this.#webSocket = undefined }
+  #websocketServerError($error) { console.error($error) }
+  #websocketOpen() {}
+  #websocketClose() {}
+  #websocketError($error) { console.error($error) }
+  #websocketMessage($data, $isBinary) {
     iterateAdapters: 
     for(const [
       $messageAdapterName, $messageAdapter
     ] of this.messageAdapters) {
       try {
         const message = $messageAdapter.message($data, $isBinary)
-        return message(this.webSocket, $data, $isBinary)
+        const { type, detail } = message(this.#webSocket, $data, $isBinary)
+        const messageEvent = new CustomEvent(type, { detail })
+        this.dispatchEvent(messageEvent)
       }
       catch($err) { /* console.log($err) */ }
     }
@@ -98,7 +89,9 @@ export default class Socket extends EventTarget {
     if(this.#messageAdapters !== undefined) { return this.#messageAdapters }
     const messageAdapters = []
     for(const [$adapterName, $adapter] of this.#settings.messageAdapters) {
-      const adapter = new MessageAdapter($adapter, this)
+      let adapter
+      if($adapter instanceof MessageAdapter) { adapter = adapter }
+      else { adapter = new MessageAdapter($adapter, this) }
       messageAdapters.push([$adapterName, adapter])
     }
     this.#messageAdapters = messageAdapters
