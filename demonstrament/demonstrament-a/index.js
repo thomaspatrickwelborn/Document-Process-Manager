294ae25597cef077b9f6b841fd0762f9e710c49a
Thomas Patrick Welborn <thomas.patrick.welborn@outlook.com>
2025-06-01 15:45:09 -0700

    Core-Plex Update

--- demonstrament/demonstrament-a/index.js
+++
@@ -1,6 +0,0 @@
-import DPM from '../development/index.js'
-import ApplicationConfig from './application.config.js'
-const application = await new Promise(($resolve, $reject) => {
-  const _application = new Application(ApplicationConfig)
-  _application.on('ready', $resolve(_application))
-})
